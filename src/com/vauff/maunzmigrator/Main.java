a778f135ab9c3889d4022bf4311804fa31ec3d0d
Vauff <mctehkitti@gmail.com>
2020-08-02 13:05:53 -0400

    r20 > r21 migrator

--- src/com/vauff/maunzmigrator/Main.java
+++ src/com/vauff/maunzmigrator/Main.java
@@ -1,85 +1,150 @@
 package com.vauff.maunzmigrator;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import org.apache.commons.io.FileUtils;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
 
 public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
-		File configFile = new File(getJarLocation() + "config.json");
 		File serverTrackingFolder = new File(getJarLocation() + "data/services/server-tracking/");
-		JSONObject configJson = new JSONObject(FileUtils.readFileToString(configFile, "UTF-8"));
-
-		if (configJson.has("cleverbotAPIKey"))
-		{
-			configJson.remove("cleverbotAPIKey");
-		}
-
-		FileUtils.writeStringToFile(configFile, configJson.toString(2), "UTF-8");
+		JSONObject configJson = new JSONObject(FileUtils.readFileToString(new File(getJarLocation() + "config.json"), "UTF-8"));
+		MongoDatabase mongoDatabase = MongoClients.create(configJson.getJSONObject("mongoDatabase").getString("connectionString")).getDatabase(configJson.getJSONObject("mongoDatabase").getString("database"));
 
 		for (File rootFolder : serverTrackingFolder.listFiles())
 		{
+			HashMap<String, ObjectId> convertedServerDictionary = new HashMap<>();
+
+			System.out.println("Converting " + rootFolder.getName());
+
+			JSONObject json = new JSONObject(FileUtils.readFileToString(new File(getJarLocation() + "data/services/server-tracking/" + rootFolder.getName() + "/serverInfo.json"), "UTF-8"));
+			int serverNumber = 0;
+
+			while (true)
+			{
+				JSONObject object;
+
+				try
+				{
+					object = json.getJSONObject("server" + serverNumber);
+				}
+				catch (JSONException e)
+				{
+					break;
+				}
+
+				serverNumber++;
+
+				Document serverDoc = new Document();
+				boolean serverExists = false;
+
+				for (Document doc : mongoDatabase.getCollection("servers").find(and(eq("ip", object.getString("serverIP")), eq("port", object.getInt("serverPort")))))
+				{
+					serverDoc = doc;
+					serverExists = true;
+					break;
+				}
+
+				if (!serverExists)
+				{
+					serverDoc.put("enabled", object.getBoolean("enabled"));
+					serverDoc.put("ip", object.getString("serverIP"));
+					serverDoc.put("port", object.getInt("serverPort"));
+					serverDoc.put("name", object.getString("serverName"));
+					serverDoc.put("map", object.getString("lastMap"));
+					serverDoc.put("timestamp", object.getLong("timestamp"));
+					serverDoc.put("playerCount", object.getString("players"));
+					serverDoc.put("players", new ArrayList<>());
+					serverDoc.put("downtimeTimer", object.getInt("downtimeTimer"));
+					serverDoc.put("failedConnectionsThreshold", 3);
+
+					ArrayList<Document> databaseEntries = new ArrayList<>();
+
+					for (int i = 0; i < object.getJSONArray("mapDatabase").length(); i++)
+					{
+						JSONObject databaseEntryJson = object.getJSONArray("mapDatabase").getJSONObject(i);
+						Document databaseEntry = new Document();
+
+						databaseEntry.put("map", databaseEntryJson.getString("mapName"));
+						databaseEntry.put("firstPlayed", databaseEntryJson.getLong("firstPlayed"));
+						databaseEntry.put("lastPlayed", databaseEntryJson.getLong("lastPlayed"));
+
+						databaseEntries.add(databaseEntry);
+					}
+
+					serverDoc.put("mapDatabase", databaseEntries);
+
+					mongoDatabase.getCollection("servers").insertOne(serverDoc);
+				}
+
+				Document serviceDoc = new Document();
+
+				serviceDoc.put("enabled", object.getBoolean("enabled"));
+				serviceDoc.put("online", object.getInt("downtimeTimer") < object.getInt("failedConnectionsThreshold"));
+				serviceDoc.put("mapCharacterLimit", object.getBoolean("mapCharacterLimit"));
+				serviceDoc.put("lastMap", object.getString("lastMap"));
+				serviceDoc.put("serverID", mongoDatabase.getCollection("servers").find(and(eq("ip", object.getString("serverIP")), eq("port", object.getInt("serverPort")))).first().getObjectId("_id"));
+				serviceDoc.put("guildID", Long.parseLong(rootFolder.getName()));
+				serviceDoc.put("channelID", object.getLong("serverTrackingChannelID"));
+				serviceDoc.put("notifications", new ArrayList<>());
+
+				convertedServerDictionary.put("server" + serverNumber, mongoDatabase.getCollection("services").insertOne(serviceDoc).getInsertedId().asObjectId().getValue());
+			}
+
 			for (File file : rootFolder.listFiles())
 			{
-				if (file.getName().equals("serverInfo.json"))
+				if (!file.getName().equals("serverInfo.json"))
 				{
-					JSONObject originalJson = new JSONObject(FileUtils.readFileToString(file, "UTF-8"));
-					JSONObject newJson = new JSONObject();
+					json = new JSONObject(FileUtils.readFileToString(file, "UTF-8"));
+					serverNumber = 0;
 
-					newJson.put("server0", new JSONObject());
-					newJson.put("lastGuildName", originalJson.getString("lastGuildName"));
-					newJson.getJSONObject("server0").put("failedConnectionsThreshold", originalJson.getInt("failedConnectionsThreshold"));
-					newJson.getJSONObject("server0").put("downtimeTimer", originalJson.getInt("downtimeTimer"));
-					newJson.getJSONObject("server0").put("players", originalJson.getString("players"));
-					newJson.getJSONObject("server0").put("lastMap", originalJson.getString("lastMap"));
-					newJson.getJSONObject("server0").put("mapCharacterLimit", originalJson.getBoolean("mapCharacterLimit"));
-					newJson.getJSONObject("server0").put("serverIP", originalJson.getString("serverIP"));
-					newJson.getJSONObject("server0").put("serverTrackingChannelID", originalJson.getLong("serverTrackingChannelID"));
-					newJson.getJSONObject("server0").put("serverPort", originalJson.getInt("serverPort"));
-					newJson.getJSONObject("server0").put("enabled", originalJson.getBoolean("enabled"));
-					newJson.getJSONObject("server0").put("timestamp", originalJson.getLong("timestamp"));
-					newJson.getJSONObject("server0").put("mapDatabase", new JSONArray());
-
-					if (originalJson.has("serverName"))
+					while (true)
 					{
-						newJson.getJSONObject("server0").put("serverName", originalJson.getString("serverName"));
+						JSONArray array;
+
+						try
+						{
+							array = json.getJSONObject("notifications").getJSONArray("server" + serverNumber);
+						}
+						catch (JSONException e)
+						{
+							break;
+						}
+
+						serverNumber++;
+
+						Document serviceDoc = mongoDatabase.getCollection("services").find(and(eq("guildID", Long.parseLong(rootFolder.getName())), eq("_id", convertedServerDictionary.get("server" + serverNumber)))).first();
+						Document notificationDoc = new Document();
+						ArrayList<String> notifications = new ArrayList<>();
+
+						for (int i = 0; i < array.length(); i++)
+						{
+							if (!array.isNull(i))
+								notifications.add(array.getString(i));
+						}
+
+						if (notifications.size() == 0)
+							continue;
+
+						notificationDoc.put("userID", Long.parseLong(file.getName().replace(".json", "")));
+						notificationDoc.put("notifications", notifications);
+						serviceDoc.getList("notifications", Document.class).add(notificationDoc);
+
+						mongoDatabase.getCollection("services").replaceOne(eq("_id", serviceDoc.getObjectId("_id")), serviceDoc);
 					}
-					else
-					{
-						newJson.getJSONObject("server0").put("serverName", "");
-					}
-
-					for (int i = 0; i < originalJson.getJSONArray("mapDatabase").length(); i++)
-					{
-						String map = originalJson.getJSONArray("mapDatabase").getString(i);
-						JSONObject databaseEntry = new JSONObject();
-
-						databaseEntry.put("firstPlayed", 0L);
-						databaseEntry.put("mapName", map);
-						databaseEntry.put("lastPlayed", 0L);
-						newJson.getJSONObject("server0").append("mapDatabase", databaseEntry);
-					}
-
-					FileUtils.writeStringToFile(file, newJson.toString(2), "UTF-8");
-				}
-				else
-				{
-					JSONObject originalJson = new JSONObject(FileUtils.readFileToString(file, "UTF-8"));
-					JSONObject newJson = new JSONObject();
-
-					newJson.put("lastName", originalJson.getString("lastName"));
-					newJson.put("notifications", new JSONObject().put("server0", new JSONArray()));
-
-					for (int i = 0; i < originalJson.getJSONArray("notifications").length(); i++)
-					{
-						newJson.getJSONObject("notifications").getJSONArray("server0").put(originalJson.getJSONArray("notifications").getString(i));
-					}
-
-					FileUtils.writeStringToFile(file, newJson.toString(2), "UTF-8");
 				}
 			}
 		}
